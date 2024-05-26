153718217d6ec42c86bd81ded4f0fb8fc9deb4d2
weeeeebo <75579372+Theonlyhamstertoh@users.noreply.github.com>
2024-05-26 13:20:17 -0700

    swap static to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), mdx(), icon()],
-    output: "static",
+    output: "server",
     adapter: vercel({
         webAnalytics: {
             enabled: true,
