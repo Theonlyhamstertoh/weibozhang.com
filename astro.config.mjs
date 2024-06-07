758c0ecd518dbcda83c1bfa010534188081c24e2
Weibo Zhang <75579372+Theonlyhamstertoh@users.noreply.github.com>
2024-06-06 17:22:34 -0700

    added rehype-pretty-code for syntax highlighting & wrote a blog on recreating gemini loading animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,24 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel/static";
+import rehypePrettyCode from "rehype-pretty-code";
 
+const prettyCodeOptions = {
+    theme: "material-theme-palenight",
+
+    tokensMap: {},
+};
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx(), icon()],
+    integrations: [
+        tailwind(),
+        mdx({
+            syntaxHighlight: false,
+            rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+            extendMarkdownConfig: true,
+        }),
+        icon(),
+    ],
     output: "static",
     site: "https://weibozhang.com/",
     adapter: vercel({
